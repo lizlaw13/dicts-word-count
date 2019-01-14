07d7b3e4b654804e2979ba58dcb3c6ad0c8dd846
Hackbright Student <no-reply@hackbrightacademy.com>
2019-01-14 12:15:43 -0800

    word count function defined

---
+++ wordcount.py
@@ -0,0 +1,15 @@
+# put your code here.
+def word_counter(filename):
+    file_name = open(filename)
+
+    word_count = {}
+    for line in file_name:
+        line= line.rstrip()
+        words = line.split(' ')
+        for word in words:
+            word_count[word] = word_count.get(word, 0) + 1
+
+    for word, count in word_count.items():
+        print(f'{word} {count}')
+    
+word_counter('test.txt')
